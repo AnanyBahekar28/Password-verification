dc0ca3a60b6d024a341d86190b3aec1c6fde4bd7
AnanyBahekar28 <63043517+AnanyBahekar28@users.noreply.github.com>
2020-07-24 09:12:53 +0530

    Update password_validation.py

--- password_validation.py
+++ password_validation.py
@@ -1,21 +1,21 @@
 digits = 0
-special_charecter = 0
+special_character = 0
 print('A valid password should have atleast 1 digit')
 print('A valid password should have 6-16 charecters')
 print('A valid password should contain any special characters from(@!#$%^)')
 password = input('Create a valid password:   ')
 
-charecters = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-if int(len(password)) in charecters:
+characters = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+if int(len(password)) in characters:
     pass   
 else:
     print('Inappropriate charecters')
 
-special_charecters = ['!', '@', '#', '$', '%', '^']
-for charecter in password:
-    if charecter in special_charecters:
-        special_charecter = special_charecter + 1
-if special_charecter == 0:
+special_characters = ['!', '@', '#', '$', '%', '^']
+for character in password:
+    if character in special_characters:
+        special_character = special_character + 1
+if special_character == 0:
     print('None of the special charecters have been included')
 
 for x in password:
@@ -24,3 +24,7 @@ for x in password:
 if digits == 0:
     print('There are no digits')
     
+"""
+if 6 < int(len(password)) < 16:
+    print("It has appropriate characters") # my sugesstion to the code
+"""
